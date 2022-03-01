3b920acc95ed21b77ceb1a395f40f093af206969
MYCHCH515 <nam1738@naver.com>
2022-03-01 22:31:10 +0900

    오타 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,10 +10,13 @@ PROJECT_NAME=parctice1-springboot-webservice
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
-echo "> 새 애플리케이션 배포"
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
+
 echo "> $JAR_NAME 에 실행권한 추가"
 
 chmod +x $JAR_NAME
